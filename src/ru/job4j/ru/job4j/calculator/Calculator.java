94c3c00d1057848de146b7b7069eeabed0e7e9bb
Michaelerantsev <michael.erantsev@gmail.com>
2020-08-03 16:39:03 +0300

    3.2. Фиксация изменений на github [#315647]

--- src/ru/job4j/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,6 @@
 package ru.job4j.ru.job4j.calculator;
 
-public class Calculator {
+   public class Calculator {
 
     public static void main(String[] args) {
        int one= 1;
--- src/ru/job4j/ru/job4j/converter.java
+++ src/ru/job4j/ru/job4j/converter.java
@@ -1,21 +1,18 @@
-package ru.job4j.converter;
+package ru.job4j.ru.job4j;
 
-public class converter {
+  public class converter {
 
-    public static int rubleToEuro(int value) {
-        int rsl = value / 70;
-        return rsl;
-    }
 
-    public static int rubleToDollar(int value) {
-        int rsl = -1; /* формула перевода рублей в доллары. */
-        return rsl;
-    }
 
-    public static void main(String[] args) {
-        int euro = converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-    }
-}
+      public static int rubleToDollar(int value) {
+          int rsl = value / 60;
+          return rsl;
+      }
 
+      public static void main(String[] args) {
+          int dollar = converter.rubleToDollar(800);
+          System.out.println("800 rubles are "+ dollar  + " dollar.");
+
+      }
+  }
 
