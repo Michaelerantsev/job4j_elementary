6df319fd88294be5b75def51d4996ba42eee1c12
Michaelerantsev <michael.erantsev@gmail.com>
2020-08-09 22:12:08 +0300

    3.2. Фиксация изменений на github [#315647]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main (String[]args){
+        int year=2020;
+       String idea="I like Java!"+"But i am a newbie."+ year;
+
+
+      System.out.println(idea);
+
+    }
+}
