0826be9e4184ae46311aa6b9fc056c6957d73ec8
Michaelerantsev <michael.erantsev@gmail.com>
2020-08-09 22:48:50 +0300

    3.2. Фиксация изменений на github [#315647]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -1,7 +1,10 @@
 package ru.job4j;
 
 public class Main {
-    public static void main (String[] args) {
+    @SuppressWarnings("checkstyle:MethodParamPad")
+    public static void main (String [] args) {
+
         System.out.println("Hello new world");
+
     }
 }
