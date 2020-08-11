d88354bccb29537022f8bf6db8ad13abc0dc44c7
Michaelerantsev <michael.erantsev@gmail.com>
2020-08-11 12:04:32 +0300

    3.2. Фиксация изменений на github [#315647]

--- test/ru/job4j/calcullate/X2Test.java
+++ test/ru/job4j/calcullate/X2Test.java
@@ -11,9 +11,9 @@ import org.junit.Test;
             public void whenBCZero() {
                 int a = 1;
                 int b = 1;
-                int c = 0;
-                int x = 1;
-                int expected = 2;
+                int c = 1;
+                int x = 0;
+                int expected = 1;
                 int rsl  = X2.calc(a, b, c, x);
                 assertThat(rsl, is(expected));
 
