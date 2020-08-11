55accc6780479cdb378994081dbd5bdfea4d7fa5
Michaelerantsev <michael.erantsev@gmail.com>
2020-08-11 11:52:53 +0300

    3.2. Фиксация изменений на github [#315647]

---
+++ test/ru/job4j/calcullate/X2Test.java
@@ -0,0 +1,21 @@
+package ru.job4j.calcullate;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+        public class X2Test {
+
+            @Test
+            public void whenBCZero() {
+                int a = 1;
+                int b = 1;
+                int c = 1;
+                int x = 1;
+                int expected = 3;
+                int rsl  = X2.calc(a, b, c, x);
+                assertThat(rsl, is(expected));
+
+            }
+        }
