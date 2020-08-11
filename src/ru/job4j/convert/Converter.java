bcac7f8b9c6758db1aa43a6648a844d791899042
Michaelerantsev <michael.erantsev@gmail.com>
2020-08-11 09:29:34 +0300

    3.2. Фиксация изменений на github [#315647]

---
+++ src/ru/job4j/convert/Converter.java
@@ -0,0 +1,19 @@
+package ru.job4j.convert;
+
+public class Converter {
+    public static int rubleToEuro(int value) {
+        int rsl = value / 70;
+        return rsl;
+    }
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(280);
+        int in = 280;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("280 rubles are " + euro + " euro.");
+        System.out.println("280 rubles are 2. Test result : " + passed);
+
+    }
+
+}
---
+++ test/ru/job4j/ru/job4j/converterTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.ru.job4j;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class converterTest {
+
+    @Test
+    public void rubleToDollar() {
+    }
+}
