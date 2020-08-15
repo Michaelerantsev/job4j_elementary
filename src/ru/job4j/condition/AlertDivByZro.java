805082e13adac8a89beae8e33e8671e684d1c850
Michaelerantsev <michael.erantsev@gmail.com>
2020-08-15 15:33:31 +0300

    3.2. Фиксация изменений на github [#315647]

--- src/ru/job4j/condition/AlertDivByZro.java
+++ src/ru/job4j/condition/AlertDivByZro.java
@@ -4,7 +4,6 @@ package ru.job4j.condition;
 
      public static void main(String[] args) {
          possibleDiv(-7);
-
      }
      public static void possibleDiv(int number){
          boolean result = number < 0;
